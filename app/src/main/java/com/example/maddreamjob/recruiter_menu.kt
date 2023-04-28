dadd836af977d286e3b1cbda6dfabea01e571039
kulan404 <kulansanjula@gmail.com>
2023-04-28 22:17:48 +0530

    add code for the repo

---
+++ app/src/main/java/com/example/maddreamjob/recruiter_menu.kt
@@ -0,0 +1,11 @@
+package com.example.maddreamjob
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class recruiter_menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recruiter_menu)
+    }
+}
---
+++ app/src/main/java/com/example/maddreamjob/seeker_menu.kt
@@ -0,0 +1,11 @@
+package com.example.maddreamjob
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class seeker_menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seeker_menu)
+    }
+}
